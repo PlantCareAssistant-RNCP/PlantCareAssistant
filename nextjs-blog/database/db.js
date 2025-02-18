d78254eeb5266d127ac9b596330f604211676f88
MarieCoulay <marie.coulay@gmail.com>
2025-02-18 11:35:34 +0100

    Tables created - database

--- nextjs-blog/database/db.js
+++ nextjs-blog/database/db.js
@@ -4,7 +4,7 @@ import { open } from 'sqlite';
 // Open a SQLite database connection
 export async function openDb() {
   return open({
-    filename: '/app/database/my-databse.db',
+    filename: '/app/database/my-database.db',
     driver: sqlite3.Database,
   });
 }
