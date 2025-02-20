50bde73833822d1841bc245a01d804b5789d7ca2
Bradley-Hill <brad.michael.hill@gmail.com>
2025-02-20 14:44:57 +0100

    dockerfile fixes

--- plant-care-assistant/app/api/users/route.ts
+++ plant-care-assistant/app/api/users/route.ts
@@ -8,6 +8,7 @@ export async function GET() {
     const users = await prisma.user.findMany();
     return NextResponse.json(users, { status: 200 });
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
       { error: "Failed to fetch users" },
       { status: 500 }
