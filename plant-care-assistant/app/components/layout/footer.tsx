8c4d0751676de52eb655edb97d2e006fd467254b
Marie Coulay <123955934+MarieCoulay@users.noreply.github.com>
2025-05-16 16:10:12 +0200

    card component (#26)

--- plant-care-assistant/app/components/layout/footer.tsx
+++ plant-care-assistant/app/components/layout/footer.tsx
@@ -5,7 +5,7 @@ import Icon from '@/components/icon'
 
 export default function Footer() {
   return (
-    <footer className="w-full relative bg-[#0A1A24] py-6">
+    <footer className="fixed bottom-0 left-0 w-full bg-[#0A1A24] py-6 z-50">
       <div className="absolute -top-8 left-1/2 transform -translate-x-1/2">
         <button className="bg-[#0A9788] w-16 h-16 rounded-full flex items-center justify-center shadow-md">
           <Icon name="photo" size={48} className="text-black" />
--- plant-care-assistant/app/components/layout/header.tsx
+++ plant-care-assistant/app/components/layout/header.tsx
@@ -6,12 +6,12 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="w-full bg-[#0A1A24] px-4 py-3 flex justify-between items-center h-16">
+    <header className="fixed top-0 left-0 w-full bg-[#0A1A24] px-4 py-3 flex justify-between items-center h-16 z-50">
       <Link href="/homepage">
-        <Icon name="home" size={35}/>
+        <Icon name="home" size={35} />
       </Link>
       <Link href="/userprofile">
-        <Icon name="dashboard" size={35}/>
+        <Icon name="dashboard" size={35} />
       </Link>
     </header>
   )
