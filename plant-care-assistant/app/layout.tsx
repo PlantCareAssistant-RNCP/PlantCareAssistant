49292bd72dfdf47702a461d7f343755a0087e739
Bradley-Hill <brad.michael.hill@gmail.com>
2025-06-04 11:16:47 +0200

    feat(auth): implement AuthProvider for user authentication and session management

--- plant-care-assistant/app/layout.tsx
+++ plant-care-assistant/app/layout.tsx
@@ -1,17 +1,20 @@
 // app/layout.tsx
-import './globals.css'
-import { ReactNode } from 'react'
-import Header from '@/components/Layout/header'
-import Footer from '@/components/Layout/footer'
+import "./globals.css";
+import { ReactNode } from "react";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
+import { AuthProvider } from "../providers/AuthProvider";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="fr">
       <body className="bg-[#06141B] text-white min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-1 overflow-y-auto">{children}</main>
-        <Footer />
+        <AuthProvider>
+          <Header />
+          <main className="flex-1 overflow-y-auto">{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
